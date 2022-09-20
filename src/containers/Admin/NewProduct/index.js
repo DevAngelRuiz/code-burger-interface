11b213bc60b49fe49134538a0b568823ef28dd07
DevAngelRuiz <angelica.a.ruiz@gmail.com>
2022-09-20 10:16:22 -0300

    logica react select e forms (controller) ok

--- src/containers/Admin/NewProduct/index.js
+++ src/containers/Admin/NewProduct/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useForm } from 'react-hook-form'
+import { useForm, Controller } from 'react-hook-form'
 import ReactSelect from 'react-select'
 
 import CloudUploadIcon from '@mui/icons-material/CloudUpload'
@@ -10,7 +10,7 @@ import { Container, Input, Label, ButtonStyles, LabelUpload } from './styles'
 function NewProduct () {
   const [fileName, setFileName] = useState(null)
   const [categories, setCategories] = useState([])
-  const { register, handleSubmit } = useForm()
+  const { register, handleSubmit, control } = useForm()
   const onSubmit = data => console.log(data)
   useEffect(() => {
     async function loadCategories () {
@@ -23,36 +23,46 @@ function NewProduct () {
   }, [])
 
   return (
-        <Container>
-            <form>
-                <Label>Nome</Label>
-                <Input type='text' {...register('name')} />
+    <Container>
+      <form>
+        <Label>Nome</Label>
+        <Input type='text' {...register('name')} />
 
-                <Label>Preço</Label>
-                <Input type='number' {...register('price')} />
+        <Label>Preço</Label>
+        <Input type='number' {...register('price')} />
 
-                <LabelUpload>
-                    { fileName || (
-                        <>
-                        <CloudUploadIcon/>
-                        Upload Imagem
-                        </>)}
-                    <input
-                        type='file' accept='image/png, image/jpg'
-                        {...register('file')}
-                        onChange={value => {
-                          setFileName(value.target.files[0]?.name)
-                        }} />
-                </LabelUpload>
+        <LabelUpload>
+          {fileName || (
+            <>
+              <CloudUploadIcon />
+              Upload Imagem
+            </>)}
+          <input
+            type='file' accept='image/png, image/jpg'
+            {...register('file')}
+            onChange={value => {
+              setFileName(value.target.files[0]?.name)
+            }} />
+        </LabelUpload>
+        <Controller
+        name='cateogry_id'
+        control={control}
+        render={({ field }) => {
+          return (
+          <ReactSelect
+          {...field}
+          options={categories}
+          getOptionLabel={cat => cat.name}
+          getOptionValue={cat => cat.id}
+          placeholder='Categorias'
+         />
+          )
+        }}
+        ></Controller>
 
-                <ReactSelect
-                options={categories}
-                getOptionLabel={cat => cat.name}
-                getOptionValue={cat => cat.id}
-                placeholder='Categorias'/>
-                <ButtonStyles>Adicionar Produto</ButtonStyles>
-            </form>
-        </Container>
+        <ButtonStyles>Adicionar Produto</ButtonStyles>
+      </form>
+    </Container>
 
   )
 }
